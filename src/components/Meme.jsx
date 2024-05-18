ed33f9626334dd4df4c8deb3ae9ef52d023acb34
Hassan Khan <hassankpisces@gmail.com>
2024-05-18 08:32:01 +0500

    Till meme image by useState

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,7 +1,28 @@
+import React from "react"
+import memesData from "../memesData.js"
+
 function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+    function getMeme() {
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        })
+        )
+    }
+
     return (
         <div className="meme">
-            <form className="form">
+            <div className="form">
                 <div className="form-group">
                     <label htmlFor="top-text" className="form-label">Top text</label>
                     <input
@@ -18,10 +39,11 @@ function Meme() {
                         className="form-input"
                         placeholder="Bottom-Text" />
                 </div>
-                <button className="form-button">
+                <button className="form-button" onClick={getMeme}>
                     Get a new meme image  🖼
                 </button>
-            </form>
+                <img className="meme-image" src={meme.randomImage} alt="" />
+            </div>
         </div>
     )
 }
